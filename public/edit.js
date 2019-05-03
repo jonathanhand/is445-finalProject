1c5ec5e27c8d9c4e0a4c02d7875c34aec457b70b
jonathanhand <hand.jonathan.13@gmail.com>
2019-05-03 11:09:42 -0700

    got promise working getUSerList and printList

---
+++ public/edit.js
@@ -0,0 +1,8 @@
+// import { testFuncVar } from './main.js';
+import { getList } from './main.js';
+// goNow();
+
+// function goNow () {
+//     console.log(testFuncVar);
+// }
+console.log(getList);
--- public/main.js
+++ public/main.js
@@ -1,62 +1,88 @@
+
 var ref = firebase.database().ref();
 var createRef = firebase.database().ref("Users/");
+// function user(id, username, phone, email) {
+//   this.id=id;
+//   this.username=username;
+//   this.phone=phone;
+//   this.email=email;
+// }
+var snapshotDB;
+function getUserList() {
+return ref.once('value').then(function (snapshot) {
+  snapshotDB = snapshot.val();
+}).then(function(snapshotDB) {
+  printList();
+  });
+}
+
+function printList () {
+console.log(snapshotDB["Users"][1].Phone);
+}
+//1105 stopped. Got the promise working. call getUserList and it gets whole array to
+//printList then printlist can do anything with the snapshotDB
+
+getUserList();
 ref.once(
   "value",
-  function(snapshot) {
-    target = document.getElementById("table1");
+  function snap (snapshot) {
+    var target = document.getElementById("table1");
 
     const data = snapshot.val().Users;
+    //console.log(data);      
+
     for (var id in data) {
-      makeTable(data[id], target);
+      //makeTable(data[id], target);
+     // addEditBtn(data[id], target);
+     //console.log(id);
     }
   },
   function(error) {
     console.log("Error: " + error.code);
   }
 );
+// function addEditBtn(dataID, target) {
+//   const editBtn = document.createElement("input");
+//   editBtn.type = "button";
+//   editBtn.className = "editBtn";
+//   editBtn.value = "Edit";
+//   editBtn.setAttribute("edit-id", dataID);
+//   target.appendChild(editBtn);
 
-function makeTable(dbData, target) {
-  var trNode = document.createElement("tr");
-  var tdName = document.createTextNode(dbData["Name"]);
-  var tdEmail = document.createTextNode(dbData["Email"]);
-  var tdPhone = document.createTextNode(dbData["Phone"]);
-  var tdNodeName = document.createElement("td");
-  var tdNodeEmail = document.createElement("td");
-  var tdNodePhone = document.createElement("td");
-  tdNodeName.appendChild(tdName);
-  tdNodeEmail.appendChild(tdEmail);
-  tdNodePhone.appendChild(tdPhone);
-  trNode.appendChild(tdNodeName);
-  trNode.appendChild(tdNodeEmail);
-  trNode.appendChild(tdNodePhone);
+//   var contactObj = [];
+//   editBtn.addEventListener("click", function(e) {
+//     contactObj = dataID;
+//       console.log(dataID);      
+//       editUser(dataID);
+//       document.location.href = "/edit.html";
+//   });
+// }
+// function getObj() {
+//     addEditBtn()
+// }
+// //add listener for edit, sends to edit page where data prefilled out
+// function makeTable(dbData, target) {
+//   var trNode = document.createElement("tr");
+//   var tdName = document.createTextNode(dbData["Name"]);
+//   var tdEmail = document.createTextNode(dbData["Email"]);
+//   var tdPhone = document.createTextNode(dbData["Phone"]);
+//   var tdNodeName = document.createElement("td");
+//   var tdNodeEmail = document.createElement("td");
+//   var tdNodePhone = document.createElement("td");
 
-  target.appendChild(trNode);
-}
 
-function createUser() {
-  ref.once(
-    //once instead of on, which would keep loading
-    "value",
-    function(snapshot) {
-      const data = snapshot.val().Users;
-      var IDLength = Object.getOwnPropertyNames(data).length;
-      writeData(IDLength);
-    },
-    function(error) {
-      console.log("Error: " + error.code);
-    }
-  );
-}
+//   tdNodeName.appendChild(tdName);
+//   tdNodeEmail.appendChild(tdEmail);
+//   tdNodePhone.appendChild(tdPhone);
+//   trNode.appendChild(tdNodeName);
+//   trNode.appendChild(tdNodeEmail);
+//   trNode.appendChild(tdNodePhone);
 
-function writeData(newID) {
-  createRef.update({
-    //appends current db
-    [newID]: {
-      Name: document.getElementById("newName").value,
-      Email: document.getElementById("newEmail").value,
-      Phone: document.getElementById("newPhone").value
-    }
-  });
-  console.log("user created");
-  location.reload(); //reloads the page after user added
-}
+//   target.appendChild(trNode);
+// }
+
+// function testFunc () {
+//   return selPer;
+// }
+
+// export const testFuncVar = testFunc();
