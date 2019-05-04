edae8ff673ab5777d6e45019ccde800d20cbdc1d
jonathanhand <hand.jonathan.13@gmail.com>
2019-05-03 23:47:24 -0700

    edit is working. updates db

--- public/edit.js
+++ public/edit.js
@@ -1,8 +1,41 @@
-// import { testFuncVar } from './main.js';
-import { getList } from './main.js';
-// goNow();
+var ref = firebase.database().ref();
+var createRef = firebase.database().ref("Users/");
 
-// function goNow () {
-//     console.log(testFuncVar);
-// }
-console.log(getList);
+var editID;
+var editObj;
+const nameField = document.getElementById("editName");
+const emailField = document.getElementById("editEmail");
+const phoneField = document.getElementById("editPhone");
+
+window.onload = function getFillFields () {
+    return ref.once('value').then(function (snapshot) {
+        editID = snapshot.val().Edit["Edit"];
+      }).then(function() {
+        return ref.once('value').then(function (snapshot) {
+            editObj = snapshot.val().Users[editID];
+            fillFields(editObj);
+
+            subEdit.addEventListener("click", function(e) {
+                overwriteData(editID, editObj);
+            });
+            })
+        });
+      }
+
+function fillFields(editObj) {
+    nameField.value = editObj["Name"];
+    emailField.value = editObj["Email"];
+    phoneField.value = editObj["Phone"];
+}
+
+function overwriteData(editID, editObj) {
+    createRef.update({
+        //appends current db
+        [editID]: {
+          Name: document.getElementById("editName").value,
+          Email: document.getElementById("editEmail").value,
+          Phone: document.getElementById("editPhone").value
+        }
+      });
+      alert("Update complete!")
+}
--- public/main.js
+++ public/main.js
@@ -1,85 +1,73 @@
 
 var ref = firebase.database().ref();
 var createRef = firebase.database().ref("Users/");
-// function user(id, username, phone, email) {
-//   this.id=id;
-//   this.username=username;
-//   this.phone=phone;
-//   this.email=email;
-// }
+var editRef = firebase.database().ref("Edit/");
+
+var target = document.getElementById("table1");
 var snapshotDB;
+
 function getUserList() {
 return ref.once('value').then(function (snapshot) {
-  snapshotDB = snapshot.val();
-}).then(function(snapshotDB) {
+  snapshotDB = snapshot.val().Users;
+}).then(function() {
   printList();
+  
   });
 }
 
 function printList () {
-console.log(snapshotDB["Users"][1].Phone);
+for (var id in snapshotDB) 
+{
+makeTable(snapshotDB[id], target);
+addEditBtn(id, target);
 }
-//1105 stopped. Got the promise working. call getUserList and it gets whole array to
-//printList then printlist can do anything with the snapshotDB
+}
 
 getUserList();
-ref.once(
-  "value",
-  function snap (snapshot) {
-    var target = document.getElementById("table1");
 
-    const data = snapshot.val().Users;
-    //console.log(data);      
+function addEditBtn(dataID, target) {
+  const editBtn = document.createElement("input");
+  editBtn.type = "button";
+  editBtn.className = "editBtn";
+  editBtn.value = "Edit";
+  editBtn.setAttribute("edit-id", dataID);
+  target.appendChild(editBtn);
 
-    for (var id in data) {
-      //makeTable(data[id], target);
-     // addEditBtn(data[id], target);
-     //console.log(id);
-    }
-  },
-  function(error) {
-    console.log("Error: " + error.code);
-  }
-);
-// function addEditBtn(dataID, target) {
-//   const editBtn = document.createElement("input");
-//   editBtn.type = "button";
-//   editBtn.className = "editBtn";
-//   editBtn.value = "Edit";
-//   editBtn.setAttribute("edit-id", dataID);
-//   target.appendChild(editBtn);
+  editBtn.addEventListener("click", function(e) {
+var selectedID = editBtn.getAttribute("edit-id");
+storeEdit(selectedID);
+  });
+}
 
-//   var contactObj = [];
-//   editBtn.addEventListener("click", function(e) {
-//     contactObj = dataID;
-//       console.log(dataID);      
-//       editUser(dataID);
-//       document.location.href = "/edit.html";
-//   });
-// }
-// function getObj() {
-//     addEditBtn()
-// }
-// //add listener for edit, sends to edit page where data prefilled out
-// function makeTable(dbData, target) {
-//   var trNode = document.createElement("tr");
-//   var tdName = document.createTextNode(dbData["Name"]);
-//   var tdEmail = document.createTextNode(dbData["Email"]);
-//   var tdPhone = document.createTextNode(dbData["Phone"]);
-//   var tdNodeName = document.createElement("td");
-//   var tdNodeEmail = document.createElement("td");
-//   var tdNodePhone = document.createElement("td");
+function storeEdit(selectedID) {
+  editRef.set({
+    //appends to stored edit id in db
+    ['Edit']: selectedID
+  });
+  window.location.href="/edit.html";
+
+}
+
+//add listener for edit, sends to edit page where data prefilled out
+function makeTable(dbData, target) {
+  var trNode = document.createElement("tr");
+  var tdName = document.createTextNode(dbData["Name"]);
+  var tdEmail = document.createTextNode(dbData["Email"]);
+  var tdPhone = document.createTextNode(dbData["Phone"]);
+  var tdNodeName = document.createElement("td");
+  var tdNodeEmail = document.createElement("td");
+  var tdNodePhone = document.createElement("td");
 
 
-//   tdNodeName.appendChild(tdName);
-//   tdNodeEmail.appendChild(tdEmail);
-//   tdNodePhone.appendChild(tdPhone);
-//   trNode.appendChild(tdNodeName);
-//   trNode.appendChild(tdNodeEmail);
-//   trNode.appendChild(tdNodePhone);
+  tdNodeName.appendChild(tdName);
+  tdNodeEmail.appendChild(tdEmail);
+  tdNodePhone.appendChild(tdPhone);
+  trNode.appendChild(tdNodeName);
+  trNode.appendChild(tdNodeEmail);
+  trNode.appendChild(tdNodePhone);
 
-//   target.appendChild(trNode);
-// }
+  target.appendChild(trNode);
+}
 
 // function testFunc () {
 //   return selPer;
